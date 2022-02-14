5046abda05126cc2777c523f48dc563dc1472d37
deivith <DAMAYA4@bancodebogota.com.co>
2022-02-13 22:37:52 -0500

    first commint

---
+++ src/test/java/com/fundamentos2/fundamentos2/Fundamentos2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentos2.fundamentos2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fundamentos2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
